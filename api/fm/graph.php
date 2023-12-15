52c771273f336190ed912aee76268fb1c0595322
Hamza <hamza.official010@gmail.com>
2023-12-15 10:04:06 +0530

    graph code logic changed

--- api/fm/graph.php
+++ api/fm/graph.php
@@ -17,31 +17,53 @@ include 'db_connection.php';
     <div>
         <canvas id="myChart"></canvas>
         <button onclick="toggleGraph()">Toggle Graph</button>
+        <button onclick="switchGraphSet('previous')">Previous Set</button>
+        <button onclick="switchGraphSet('next')">Next Set</button>
     </div>
 
     <script>
         console.log('Script is running');
         let currentGraph = 'A'; // Initial graph type
+        let currentDataSetIndex = 0; // Index of the current data set
         let chartInstance; // Store the Chart.js instance
+        let sensorSets; // Array to store sets of 1 to 8 sensor data
 
         fetch('fm_api.php')
             .then(response => response.json())
             .then(data => {
                 // Use the fetched data here
                 console.log(data);
-                createChart(data);
+                // Divide the data into sets of 1 to 8 sensors
+                sensorSets = divideDataIntoSets(data.data, 8);
+                createChart(sensorSets[currentDataSetIndex]);
             })
             .catch(error => console.error('Error fetching data:', error));
 
+        function divideDataIntoSets(data, sensorsPerSet) {
+            const sets = [];
+            for (let i = 0; i < data.length; i += sensorsPerSet) {
+                sets.push(data.slice(i, i + sensorsPerSet));
+            }
+            return sets;
+        }
+
         function createChart(data) {
+            console.log('Creating chart...');
+
             const seriesCollection = [];
-            const uniqueSensors = Array.from(new Set(data.data.map(item => parseInt(item.sensor))));
-            const maxY = uniqueSensors.length * 5;
+            const maxY = data.length * 5;
+
+            // Define depth values for each sensor
+            const depthValues = data.reduce((acc, item, index) => {
+                const sensor = parseInt(item.sensor);
+                acc[sensor] = -5 * index;
+                return acc;
+            }, {});
 
             // Loop through selected items
-            data.data.forEach(item => {
+            data.forEach(item => {
                 const sensor = parseInt(item.sensor);
-                const depth = (uniqueSensors.length - sensor) * 5;
+                const depth = depthValues[sensor];
                 const value = currentGraph === 'A' ? parseFloat(item.value1) : parseFloat(item.value2);
 
                 if (!seriesCollection[sensor]) {
@@ -56,12 +78,16 @@ include 'db_connection.php';
                 }
 
                 // Populate series with data points
-                seriesCollection[sensor].values.push({ x: value, y: depth });
+                seriesCollection[sensor].values.push({
+                    x: value,
+                    y: depth,
+                    label: `Value: ${value}\nDate Time: ${item.date_time}`
+                });
             });
-// ----------------------------
+
             // Print seriesCollection to the console
-    console.log('Series Collection:', seriesCollection);
-// ------------------------------
+            console.log('Series Collection:', seriesCollection);
+
             const config = {
                 type: 'line',
                 data: {
@@ -76,7 +102,7 @@ include 'db_connection.php';
                             min: -40,
                             max: 40,
                             ticks: {
-                                stepSize: 10,
+                                stepSize: 5,
                             },
                             title: {
                                 display: true,
@@ -86,12 +112,12 @@ include 'db_connection.php';
                         y: {
                             type: 'linear',
                             position: 'left',
-                            min: 0,
-                            max: maxY,
+                            min: -maxY, // Make min negative to reverse the ticks
+                            max: 0,
                             ticks: {
                                 stepSize: 5,
-                                reverse: true,
-                                callback: function (value, index, values) {
+                                reverse: false, // Set reverse to false
+                                callback: function(value, index, values) {
                                     return Math.abs(value) + 'm';
                                 },
                             },
@@ -101,22 +127,42 @@ include 'db_connection.php';
                             },
                         },
                     },
+                    plugins: {
+                        tooltip: {
+                            callbacks: {
+                                label: function(context) {
+                                    const label = context.dataset.label || '';
+                                    if (label) {
+                                        return `${label}`;
+                                    }
+                                    return null;
+                                },
+                            },
+                        },
+                    },
                 },
             };
+            console.log('Chart config:', config);
 
             const ctx = document.getElementById('myChart').getContext('2d');
             chartInstance = new Chart(ctx, config);
         }
 
         function toggleGraph() {
+            console.log("inside toggleGraph function");
             currentGraph = currentGraph === 'A' ? 'B' : 'A';
             chartInstance.destroy(); // Destroy the current chart instance
-            fetch('fm_api.php')
-                .then(response => response.json())
-                .then(data => {
-                    createChart(data);
-                })
-                .catch(error => console.error('Error fetching data:', error));
+            createChart(sensorSets[currentDataSetIndex]);
+        }
+
+        function switchGraphSet(direction) {
+            if (direction === 'previous' && currentDataSetIndex > 0) {
+                currentDataSetIndex--;
+            } else if (direction === 'next' && currentDataSetIndex < sensorSets.length - 1) {
+                currentDataSetIndex++;
+            }
+            chartInstance.destroy(); // Destroy the current chart instance
+            createChart(sensorSets[currentDataSetIndex]);
         }
 
         // Function to generate random color
