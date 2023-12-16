e7d807cb61078e322ea2db5a0344e0abdd8c4554
Hamza Haider <123259911+Hamza-H10@users.noreply.github.com>
2023-12-16 17:46:45 +0530

    testing fils for graph debugging, test: some code changes

--- api/fm/testing.php
+++ api/fm/testing.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Chart.js Line Plot</title>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
 
@@ -14,37 +14,88 @@
     </div>
 
     <script>
-        // Fetch data from server
-        fetch('fm_api.php')
-            .then(response => response.json())
-            .then(data => {
-                // Process data and update chart
-                updateChart(data);
-            })
-            .catch(error => console.error('Error fetching data:', error));
+function createChart(data) {
+    const cumulativeNegativeDepth = data.map(item => item['Cumulative Negative Depth']);
+    const aValues = data.map(item => item['A']);
 
-        // Function to update the chart with the fetched data
-        function updateChart(data) {
-            // Process data and update chart as needed
-            // Example: Assume data is an array of values
-            const labels = [...]; // Set labels based on data
-            const chartData = {
-                labels: labels,
-                datasets: [{
-                    label: 'My Dataset',
-                    data: data,
-                    // Additional chart options...
-                }]
-            };
+    const config = {
+        type: 'line',
+        data: {
+            labels: aValues,
+            datasets: [{
+                label: 'Cumulative Negative Depth vs A',
+                data: cumulativeNegativeDepth,
+                borderColor: 'rgba(75, 192, 192, 100)',
+                borderWidth: 1,
+                pointRadius: 5,
+                // pointBackgroundColor: 'rgba(75, 192, 192, 1)',
+                fill: false,
+                tension: 0.2,
+                spanGaps: false
+            }],
+        },
+        options: {
+    scales: {
+        x: {
+            type: 'linear',
+            position: 'bottom',
+            ticks: {
+                stepSize: 5,
+                callback: function (value) {
+                    return value % 10 === 0 ? value : '\u200B'; // Add zero-width space for non-labeled ticks
+                },
+            },
+            max: 40, // Set the maximum value for the x-axis
+            min: -40, // Set the minimum value for the x-axis
+            suggestedMax: 40, // Suggested maximum value for the x-axis
+            suggestedMin: -40, // Suggested minimum value for the x-axis
+            grid: {
+                color: (context) => context.tick.value === 0 ? 'rgba(0,0,0,1)' : 'rgba(0,0,0,0.1)',
+                lineWidth: (context) => context.tick.value === 0 ? 2 : 1,
+            },
+            title: {
+                display: true,
+                text: 'A',
+            },
+        },
+        y: {
+            title: {
+                display: true,
+                text: 'Cumulative Negative Depth',
+            },
+        },
+    },
+},
 
-            // Create or update the chart
-            const ctx = document.getElementById('myChart').getContext('2d');
-            const myChart = new Chart(ctx, {
-                type: 'line',
-                data: chartData,
-                // Additional chart options...
-            });
-        }
+    };
+
+    const ctx = document.getElementById('myChart').getContext('2d');
+    new Chart(ctx, config);
+}
+
+
+
+        // Test Data
+        const testData = [
+            { 'Cumulative Negative Depth': -18.0, 'A': 1.63 },
+            { 'Cumulative Negative Depth': -36.0, 'A': -0.74 },
+            { 'Cumulative Negative Depth': -60.0, 'A': -2.07 },
+            { 'Cumulative Negative Depth': -90.0, 'A': -2.32 },
+            { 'Cumulative Negative Depth': -126.0, 'A': 2.23 },
+            { 'Cumulative Negative Depth': -168.0, 'A': -1.78 },
+            { 'Cumulative Negative Depth': -216.0, 'A': -1.93 },
+            { 'Cumulative Negative Depth': -222.0, 'A': 7.05 },
+            { 'Cumulative Negative Depth': -234.0, 'A': 1.63 },
+            { 'Cumulative Negative Depth': -252.0, 'A': -0.76 },
+            { 'Cumulative Negative Depth': -276.0, 'A': -2.08 },
+            { 'Cumulative Negative Depth': -306.0, 'A': -2.33 },
+            { 'Cumulative Negative Depth': -342.0, 'A': 2.21 },
+            { 'Cumulative Negative Depth': -384.0, 'A': -1.81 },
+            { 'Cumulative Negative Depth': -432.0, 'A': -1.94 },
+        ];
+
+        // Call the createChart function with test data
+        createChart(testData);
     </script>
 </body>
 
--- api/fm/testing2.php
+++ api/fm/testing2.php
@@ -26,11 +26,10 @@
         fetch('fm_api.php')
             .then(response => response.json())
             .then(data => {
-                // Use the fetched data here
-                console.log(data);
-                // Divide the data into sets of 1 to 8 sensors
-                sensorSets = divideDataIntoSets(data.data, 8);
-                createChart(sensorSets[currentDataSetIndex]);
+                const sensorSets = divideDataIntoSets(data.data, 8); // Divide data into sets
+                sensorSets.forEach((data, index) => {
+                    createChart(data, index); // Call createChart for each set with its data and index
+                });
             })
             .catch(error => console.error('Error fetching data:', error));
 
@@ -42,8 +41,8 @@
             return sets;
         }
 
-        function createChart(data) {
-            console.log('Creating chart...');
+        function createChart(data, index) {
+            console.log(`Creating chart for set ${index + 1}...`);
 
             const seriesCollection = [];
             const maxY = data.length * 5;
@@ -68,7 +67,8 @@
                         borderColor: getRandomColor(),
                         borderWidth: 1,
                         pointRadius: 5,
-                        type: 'line',
+                        type: 'line', // Added line type
+                        tension: 0.5,
                     };
                 }
                 // Populate series with data points
@@ -79,68 +79,67 @@
             console.log('Series Collection:', seriesCollection);
 
             const config = {
-    type: 'line',
-    data: {
-        datasets: Object.values(seriesCollection),
-    },
-    options: {
-        scales: {
-            x: {
-                type: 'linear',
-                position: 'bottom',
-                min: -60,
-                max: 60,
-                ticks: {
-                    stepSize: 5,
-                    callback: function (value) {
-                        return value % 10 === 0 ? value : '\u200B'; // Add zero-width space for non-labeled ticks
+                type: 'line',
+                data: {
+                    datasets: Object.values(seriesCollection),
+                },
+                options: {
+                    scales: {
+                        x: {
+                            type: 'linear',
+                            position: 'bottom',
+                            min: -60,
+                            max: 60,
+                            ticks: {
+                                stepSize: 5,
+                                callback: function (value) {
+                                    return value % 10 === 0 ? value : '\u200B'; // Add zero-width space for non-labeled ticks
+                                },
+                            },
+                            title: {
+                                display: true,
+                                text: currentGraph === 'A' ? 'Displacement A (Deg)' : 'Displacement B (Deg)',
+                                font: {
+                                    size: 14,
+                                    weight: 'bold'
+                                }
+                            },
+                            grid: {
+                                color: (context) => context.tick.value === 0 ? 'rgba(0,0,0,1)' : 'rgba(0,0,0,0.1)',
+                                lineWidth: (context) => context.tick.value === 0 ? 0.75 : 1,
+                            },
+                        },
+                        y: {
+                            type: 'linear',
+                            position: 'left',
+                            min: -maxY,
+                            max: 0,
+                            ticks: {
+                                stepSize: 5,
+                                reverse: false,
+                                callback: function (value, index, values) {
+                                    return Math.abs(value) + 'm';
+                                },
+                                font: {
+                                    size: 12,
+                                    weight: 'bold'
+                                }
+                            },
+                            title: {
+                                display: true,
+                                text: 'Depth (m)',
+                                font: {
+                                    size: 14,
+                                    weight: 'bold'
+                                }
+                            },
+                        },
                     },
                 },
-                title: {
-                    display: true,
-                    text: currentGraph === 'A' ? 'Displacement A (Deg)' : 'Displacement B (Deg)',
-                    font: {
-                        size: 14,
-                        weight: 'bold'
-                    }
-                },
-                grid: {
-                    color: (context) => context.tick.value === 0 ? 'rgba(0,0,0,1)' : 'rgba(0,0,0,0.1)',
-                    lineWidth: (context) => context.tick.value === 0 ? 0.75 : 1,
-                },
-            },
-            y: {
-                type: 'linear',
-                position: 'left',
-                min: -maxY,
-                max: 0,
-                ticks: {
-                    stepSize: 5,
-                    reverse: false,
-                    callback: function (value, index, values) {
-                        return Math.abs(value) + 'm';
-                    },
-                    font: {
-                        size: 12,
-                        weight: 'bold'
-                    }
-                },
-                title: {
-                    display: true,
-                    text: 'Depth (m)',
-                    font: {
-                        size: 14,
-                        weight: 'bold'
-                    }
-                },
-            },
-        },
-    },
-};
+            };
 
-const ctx = document.getElementById('myChart').getContext('2d');
-chartInstance = new Chart(ctx, config);
-
+            const ctx = document.getElementById('myChart').getContext('2d');
+            chartInstance = new Chart(ctx, config);
         }
 
         function toggleGraph() {
